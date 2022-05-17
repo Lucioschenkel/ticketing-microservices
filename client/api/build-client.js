3de26e41c214d833d53d13ae6117582ff6a5e3c4
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2022-05-17 12:09:07 -0300

    updates ingress-ngnix config

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'https://ticketing-microservices-dev-lcio.live',
       headers: req.headers,
     });
   } else {
